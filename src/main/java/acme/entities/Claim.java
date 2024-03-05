301e77bd4c7bb8bccc87a4ac1a4630b647b4c248
javulegar <javiulecia@gmail.com>
2024-03-05 12:25:48 +0100

    Feature 3 create claim

---
+++ src/main/java/acme/entities/Claim.java
@@ -0,0 +1,55 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Pattern(regexp = "C-[0-9]{4}")
+	@NotBlank
+	@Column(unique = true)
+	String						code;
+
+	@NotBlank
+	@Temporal(TemporalType.TIMESTAMP)
+	Date						instantiation;
+
+	@NotBlank
+	@Length(min = 0, max = 75)
+	String						heading;
+
+	@NotBlank
+	@Length(min = 0, max = 100)
+	String						description;
+
+	@NotBlank
+	@Length(min = 0, max = 100)
+	String						department;
+
+	@Email
+	String						email;
+
+	@URL
+	String						link;
+
+}
