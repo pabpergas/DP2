2f0b39e51289631372fb0ff1f94565d77f506294
romerito3rubn <romerito3rubn@gmail.com>
2024-03-06 18:45:15 +0100

    feat: AdministratorDashboard

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,26 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdministratorDashboard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	int							totalPrincipalsWithRole;
+	Double						ratioNoticesWithEmailAndLink;
+	Double						ratioCriticalObjectives;
+	Double						ratioNonCriticalObjectives;
+	Double						averageValueInRisks;
+	Double						minimumValueInRisks;
+	Double						maximumValueInRisks;
+	Double						standardDeviationValueInRisks;
+	Double						averageNumberOfClaimsLast10Weeks;
+	Double						minimumNumberOfClaimsLast10Weeks;
+	Double						maximumNumberOfClaimsLast10Weeks;
+	Double						standardDeviationNumberOfClaimsLast10Weeks;
+}
