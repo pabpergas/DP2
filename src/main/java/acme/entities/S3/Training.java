9642cf528037c18142ae11f22271661336635b86
Rafael Duque <91947361+rafduqcol@users.noreply.github.com>
2024-03-07 17:29:02 +0100

    Update Training.java

--- src/main/java/acme/entities/S3/Training.java
+++ src/main/java/acme/entities/S3/Training.java
@@ -11,7 +11,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -28,36 +28,29 @@ import lombok.Setter;
 public class Training extends AbstractEntity {
 
 	@NotBlank
-	@NotNull
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "The code must be in the correct format: [A-Z]{1,3}-[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$")
 	@Column(unique = true)
 	private String		code;
 
 	@NotNull
-	@Past
+	@PastOrPresent
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date		moment;
 
 	@NotBlank
-	@NotNull
 	@Length(max = 100)
 	private String		details;
 
 	@NotBlank
-	@NotNull
-	@Enumerated(EnumType.STRING)
 	private Difficulty	difficulty;
 
 	@URL
 	private String		link;
 
-	@Past
+	@PastOrPresent
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date		updateMoment;
 
-	@NotNull
-	@Column(name = "estimated_total_time")
-	private int			estimatedTotalTime; // Represented in minutes
+	private int			estimatedTotalTime; 
 
-	// Constructors, getters, and setters can be added here
 }
